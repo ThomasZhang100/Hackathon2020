e86e09f481beb426a76730f3c7a87ac16e975c5c
Thomas Zhang <thomaszhang@Thomass-MBP.fios-router.home>
2020-11-07 20:03:15 -0500

    integrated flask with html run main.py

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/grade9")
+def grade9():
+    return render_template("grade9.html")
+
+@app.route("/grade10")
+def grade10():
+    return render_template("grade10.html")
+
+@app.route("/grade11")
+def grade11():
+    return render_template("grade11.html")
+
+@app.route("/grade12")
+def grade12():
+    return render_template("grade12.html")
+
+@app.route("/electives")
+def electives():
+    return render_template("electives.html")
 
 if __name__ == "__main__":
     app.run()
