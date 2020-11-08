f476a37d8953dc6c01049c1241d93736f635d8cd
mistertooth2005 <btaylor3240@gmail.com>
2020-11-07 21:51:10 -0500

    working on middle tier

---
+++ website/init_db.py
@@ -0,0 +1,3 @@
+from models import Teacher, Review
+from __init__ import db
+db.create_all()
---
+++ website/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from __init__ import db
+
+
+class Teacher(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	reviews = db.relationship('Review', backref='teacher', lazy=True)
+
+	def __repr__(self):
+		return f"User('{self.name}', '{self.image_file}')"
+
+class Review(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	# date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	content = db.Column(db.Text, nullable=False)
+	teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), nullable=False)
+
+	def __repr__(self):
+		return f"Post('{self.content}')"
